ac8e59ef25791aec02931ae170a7fa47ba6a684f
Greg Holmes <greg.holmes2@vonage.com>
2020-08-24 12:45:45 +0100

    Removed  and any container useages in index.php as no longer required in Slim4

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-use DI\Container;
+
 use Nexmo\Client\Exception\Request as NexmoRequestException;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -8,28 +8,21 @@ use Slim\Factory\AppFactory;
 require '../vendor/autoload.php';
 require '../config.php';
 
-$container = new Container();
-AppFactory::setContainer($container);
-
 $app = AppFactory::create();
-$container->set('config', $config);
 
-// Register component on container
-$container->set('view', function () {
-    return new \Slim\Views\PhpRenderer('../templates/');
+$view = new \Slim\Views\PhpRenderer('../templates/');
+
+$app->get('/', function (Request $request, Response $response) use ($view) {
+    return $view->render($response, 'main.php', ['error' => false, 'insight' => null]);
 });
 
-$app->get('/', function (Request $request, Response $response) {
-    return $this->get('view')->render($response, 'main.php', ['error' => false, 'insight' => null]);
-});
-
-$app->post('/insight', function (Request $request, Response $response) {
+$app->post('/insight', function (Request $request, Response $response) use ($view, $config) {
     try {
         $params = $request->getParsedBody();
 
         $basic = new \Nexmo\Client\Credentials\Basic(
-            $this->get('config')['api_key'],
-            $this->get('config')['api_secret']
+            $config['api_key'],
+            $config['api_secret']
         );
         $client = new \Nexmo\Client($basic);
 
@@ -47,10 +40,10 @@ $app->post('/insight', function (Request $request, Response $response) {
         }
 
         if ($insight && $insight['status'] == 0) {
-            return $this->get('view')->render($response, 'main.php', ['insight' => $insight]);
+            return $view->render($response, 'main.php', ['insight' => $insight]);
         }
     } catch (NexmoRequestException $requestError) {
-        return $this->get('view')->render(
+        return $view->render(
             $response, 
             'main.php', 
             [
